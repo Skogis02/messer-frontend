b8082a4b48ce1347800887d469f5f66bb1bd4980
isac <isacsk@kth.se>
2022-11-13 16:02:23 +0100

    Added features for sending messages.

--- src/components/ChatInputField.tsx
+++ src/components/ChatInputField.tsx
@@ -1,16 +1,40 @@
-import React from 'react'
+import React, { ChangeEvent, useState } from 'react'
+import { useAuthContext } from '../contexts/AuthContext'
+import { useFriendContext } from '../contexts/FriendContex'
 import './styles/ChatInputField.css'
 
 export const ChatInputField: React.FC = () => {
+  const [message, setMessage] = useState<string>('')
+
+  const authContext = useAuthContext()
+  const friendContext = useFriendContext()
+
+  const handleSumbit = () => {
+    authContext.useSocket().send(
+      JSON.stringify({
+        endpoint: 'send_message',
+        content: {
+          friend: friendContext.selectedFriend,
+          content: message
+        }
+      })
+    )
+    setMessage('')
+  }
+
   return (
     <div
     className='chat-input-field'
     >
         <input
+        onChange={((event: ChangeEvent<HTMLInputElement>) => setMessage(event.target.value))}
         className='univ-input'
-        type='text'>
+        type='text'
+        value={message}
+        >
         </input>
         <button
+        onClick={handleSumbit}
         className='send-button'
         >
           Send!
